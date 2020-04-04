bd6a7885f91c8d2793089e56ce66c579f2d9a2a7
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-03 21:09:50 -0500

    Update TitleCountSpark.py

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -9,12 +9,10 @@ import re
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
-
 with open(stopWordsPath) as f:
 	#TODO
 	stopWords = f.readlines()
 		
-
 with open(delimitersPath) as f:
 	#TODO
 	delimiters = f.read()
@@ -27,19 +25,16 @@ lines = sc.textFile(sys.argv[3],1)
 
 #TODO
 words = lines.flatMap(lambda line: re.split(delimiters, line)) #split lines and add delimited line to counts RDD
-counts = words.filter(lambda word: word.lower() not in stopWords) \
-		.map(lambda word: (word, 1)) \
-		.reduceByKey(lambda a, b: a + b)
+counts = words.filter(lambda word: word.lower() not in stopWords).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 #swap key and val so that key is count and value is word. Then sort in descending order first
-counts = counts.map(lambda w: (w[1], w[0])) \
-		.sortByKey(ascending = False)
+sortedCounts = counts.map(lambda w: (w[1], w[0])).sortByKey(ascending = False)
 
 outputFile = open(sys.argv[4],"w")
 
 #TODO
 #write results to output file. Foramt for each line: (line +"\n")
 topTen = ""
-for word in counts.take(10):
+for word in sortedCounts.take(10):
 	topTen = word[1] + "\t" + str(word[0]) + "\n" + topTen
 outputFile.write(topTen)
 outputFile.close()
