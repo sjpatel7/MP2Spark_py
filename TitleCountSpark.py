b4515507a4c44ce2bea72c6808131fe615edd463
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-03 21:24:49 -0500

    Update TitleCountSpark.py

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -25,16 +25,19 @@ lines = sc.textFile(sys.argv[3],1)
 
 #TODO
 words = lines.flatMap(lambda line: re.split(delimiters, line)) #split lines and add delimited line to counts RDD
-counts = words.filter(lambda word: word.lower() not in stopWords).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+counts = words.filter(lambda word: word.lower() not in stopWords) \
+		.map(lambda word: (word, 1)) \
+		.reduceByKey(lambda a, b: a + b)
 #swap key and val so that key is count and value is word. Then sort in descending order first
-sortedCounts = counts.map(lambda w: (w[1], w[0])).sortByKey(ascending = False)
+counts = counts.map(lambda w: (w[1], w[0])) \
+		.sortByKey(ascending = False)
 
 outputFile = open(sys.argv[4],"w")
 
 #TODO
 #write results to output file. Foramt for each line: (line +"\n")
 topTen = ""
-for word in sortedCounts.take(10):
+for word in counts.take(10):
 	topTen = word[1] + "\t" + str(word[0]) + "\n" + topTen
 outputFile.write(topTen)
 outputFile.close()
