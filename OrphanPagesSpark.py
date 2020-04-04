f04406c99d7e3988569274e0ed42f5882359645b
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-04 04:09:22 -0500

    Update OrphanPagesSpark.py

--- OrphanPagesSpark.py
+++ OrphanPagesSpark.py
@@ -8,10 +8,37 @@ sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
+
 #TODO
+def getPages(line):
+  line = line.rstrip()
+  line = line.split(":")
+  line = [int(line[0])] + list(map(int, line[1].split(" ")))
+  line[0] = -line[0]
+  return line
+
+def page_kv(line):
+  pair = line.split(": ")
+  key = int(pair[0])
+  val = list(map(int, pair[1].split(" ")))
+  return (key, val)
+
+def getVal(page):
+  if page < 0:
+    return (abs(page), 1) #possible orphan
+  else:
+    return (page, 0) #child
+  
+orphans = lines.flatMap(lambda line: getPages(line)) \
+                .map(lambda p: getVal(p)) \
+                .reduceByKey(lambda a, b: a * b) \ #0 if child, 1 if orphan
+                .filter(lambda p: p[1] == 1)
+                .sortByKey(ascending = True)
 
 output = open(sys.argv[2], "w")
 
+for orphan in orphans.collect():
+  output.write(orphan[0] + "\n")
 #TODO
 #write results to output file. Foramt for each line: (line+"\n")
 
