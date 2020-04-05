2dc41acc70d4db7ff969c5c8c1283053da86b60a
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-04 21:53:23 -0500

    Update PopularityLeagueSpark.py

--- PopularityLeagueSpark.py
+++ PopularityLeagueSpark.py
@@ -11,15 +11,60 @@ sc = SparkContext(conf = conf)
 lines = sc.textFile(sys.argv[1], 1) 
 
 #TODO
+#count incoming links same way as part D
+#then filter based on key in leagueIDS 
+def getLinks(line):
+  line = line.split(':')
+  links = line[1].split(" ")
+  for val in links:
+    if not val.isdigit():
+      links.remove(val)
+  return links
 
+counts = lines.flatMap(lambda line: getLinks(line)) \
+              .map(lambda link: (link, 1)) \
+              .reduceByKey(lambda a, b: a + b) \
+
+#each line has a league ID
 leagueIds = sc.textFile(sys.argv[2], 1)
 
 #TODO
+leagueIds = leagueIds.collect()
+for i in range(len(leagueIds)):
+  leagueIds[i] = leagueIds[i].split()
+leagueCounts = counts.filter(lambda p: p[0] in leagueIds) \
+                      .map(lambda p: (p[1], p[0])) \
+                      .sortByKey(ascending=True)
+
+#w
+pairs = leagueCounts.collect() #list of (count (int), id (str)) pairs in ascending count order
+#calculate rank and then sort by id alphabetical order
+ranks = {}
+rankC = 0
+lastVal = -1
+buffer = 0
+for p in pairs:
+  key = p[1]
+  val = p[0]
+  #if initial pair
+  if lastVal == -1:
+    lastVal = val
+    ranks[key] = rankC
+    continue
+  #if lastval same as current val, buffer++ and don't change rank (assign same rank as last)
+  if lastVal == val:
+    ranks[key] = rankC
+    buffer += 1
+  else:
+    rankC += 1
+    ranks[key] = rankC + buffer
+    lastVal = val
 
 output = open(sys.argv[3], "w")
-
 #TODO
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
-
+for key in sorted(ranks):
+  output.write(str(key) + '\t' + str(ranks[key]) + '\n'
+output.close()
 sc.stop()
 
