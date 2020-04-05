8408535feea59881b32eef985c556b11a2f10874
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-04 23:20:24 -0500

    update output to be lexicographical

--- OrphanPagesSpark.py
+++ OrphanPagesSpark.py
@@ -30,7 +30,7 @@ orphans = lines.flatMap(lambda line: getPages(line)) \
                 .sortByKey(ascending = True)
 output = open(sys.argv[2], "w")
 
-for orphan in orphans.collect():
+for orphan in sorted(orphans.collect()):
   output.write(str(orphan[0]) + "\n")
 #TODO
 #write results to output file. Foramt for each line: (line+"\n")
